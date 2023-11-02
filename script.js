8c63576888583ff9f7720beda414cca2351da9fe
Thomas One <103159730+tommyone99@users.noreply.github.com>
2023-11-02 20:59:57 +0000

    create 16 by 16 grid

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const canvas = document.querySelector("#canvas");
 
 for (let i = 0; i < 16; i++) {
-    const div = document.createElement("div");
-    div.style.width = "60px";
-    div.style.height = "60px";
-    div.style.border = "black solid 1px";
-    canvas.appendChild(div);
+    const row = document.createElement("div");
+    row.style.display = "row";
+    row.classList.toggle('row');
+        for (let i = 0; i < 16; i++) {
+            const div = document.createElement("div");
+            div.style.width = "60px";
+            div.style.height = "60px";
+            div.style.border = "black solid 1px";
+            row.appendChild(div);
+        };
+    canvas.appendChild(row);
 };
