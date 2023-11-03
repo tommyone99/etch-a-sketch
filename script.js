1a3930cff17c918af49ec0c77d1f0c6fbc0cf321
Thomas One <103159730+tommyone99@users.noreply.github.com>
2023-11-03 01:33:38 +0000

    move main code into 'createGrid' function with a variable loop instead of 16

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
 const canvas = document.querySelector("#canvas");
-const button = document.querySelector("#newGrid");
+const button = document.querySelector("#new-grid");
+
+let n = 16;
 
 button.addEventListener("click", newGrid);
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.style.display = "row";
-    row.classList.toggle('row');
-        for (let i = 0; i < 16; i++) {
-            const div = document.createElement("div");
-            div.style.width = "60px";
-            div.style.height = "60px";
-            div.style.border = "black solid 1px";
-            div.addEventListener("mousemove", bgBlack);
-            row.appendChild(div);
-        };
-    canvas.appendChild(row);
+
+createGrid(n);
+
+
+
+
+
+
+function createGrid(n) {
+    for (let i = 0; i < n; i++) {
+        const row = document.createElement("div");
+        row.style.display = "row";
+        row.classList.toggle('row');
+            for (let i = 0; i < n; i++) {
+                const div = document.createElement("div");
+                div.style.width = "60px";
+                div.style.height = "60px";
+                div.style.border = "black solid 1px";
+                div.addEventListener("mousemove", bgBlack);
+                row.appendChild(div);
+            };
+        canvas.appendChild(row);
+    };
 };
 
 function bgBlack(e) {
     e.target.style.backgroundColor = "black";
+}
+
+function clearGrid() {
+    while (canvas.firstChild){
+        canvas.removeChild(canvas.lastChild);
+    }
+}
+
+function newGrid() {
+    n = window.prompt("number of squares per side for the new grid", 16);
+    clearGrid();
+    createGrid(n);
 }
